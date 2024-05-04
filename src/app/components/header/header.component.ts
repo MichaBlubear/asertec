e4d7d4971c01403bcf41422dfa2a6142d5d53089
Michael Sarmiento <michael@blubear.io>
2024-05-04 00:44:17 -0500

    FEAT: heder flotaqnte

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @ViewChild('elementoPin') elementoPin!: ElementRef;
 
+  constructor() {} // Constructor vacío
+  
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const elementoSuperior = document.querySelector('.bg-blue-500');
+    const elementoPin = this.elementoPin.nativeElement;
+    const elementoSuperiorRect = elementoSuperior!.getBoundingClientRect();
+    
+    if (elementoSuperiorRect.bottom <= 0) {
+      elementoPin.classList.add('fixed', 'top-0', 'left-0', 'right-0', 'z-10');
+    } else {
+      elementoPin.classList.remove('fixed', 'top-0', 'left-0', 'right-0', 'z-10');
+    }
+  }
 }
