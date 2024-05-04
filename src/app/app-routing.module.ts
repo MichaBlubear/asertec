1ca7fa0f6d7e4412d776d877e64eecbe7f38c524
Michael Sarmiento <michael@blubear.io>
2024-05-04 00:13:50 -0500

    FEAT: rutas, servicios, componentes principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { SolutionsComponent } from './pages/solutions/solutions.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: HomeComponent },
+  { path: 'nosotros', component: AboutUsComponent },
+  { path: 'soluciones', component: SolutionsComponent },
+  { path: 'contactanos', component: ContactUsComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { SolutionsComponent } from './pages/solutions/solutions.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    SolutionsComponent,
+    ContactUsComponent,
   ],
   imports: [
     BrowserModule,
